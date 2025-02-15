8b8a67ca879b25a95e786742e07a8a93ffdee4b6
Anna Fedyna <annafedyna03@gmail.com>
2025-02-15 17:29:31 +0000

    form setup and generate options for users

---
+++ script.js
@@ -0,0 +1,18 @@
+// This is a placeholder file which shows how you can access functions defined in other files.
+// It can be loaded into index.html.
+// You can delete the contents of the file once you have understood how it works.
+// Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
+// You can't open the index.html file using a file:// URL.
+
+import { getUserIds } from "./storage.js";
+
+window.onload = function () {
+  const users = getUserIds();
+  const userSelect = document.querySelector("#user-select");
+  for (let i = 0; i < users.length; i++) {
+    const newOption = document.createElement("option");
+    newOption.value = i;
+    newOption.innerHTML = `User ${users[i]}`;
+    userSelect.appendChild(newOption);
+  }
+};
