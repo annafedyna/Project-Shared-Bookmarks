e830a6002fe11e72df13af78b22b3b58bbd1a3bc
Anna Fedyna <annafedyna03@gmail.com>
2025-02-16 23:21:59 +0000

    feac: additional functionality

--- script.js
+++ script.js
@@ -4,7 +4,8 @@
 // Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
 // You can't open the index.html file using a file:// URL.
 
-import { getUserIds } from "./storage.js";
+import { getUserIds, getData, setData, clearData } from "./storage.js";
+import { renderBookmarksUser } from "./renderBookmarks.js";
 
 window.onload = function () {
   const users = getUserIds();
@@ -13,6 +14,47 @@ window.onload = function () {
     const newOption = document.createElement("option");
     newOption.value = i;
     newOption.innerHTML = `User ${users[i]}`;
-    userSelect.appendChild(newOption);
+    userSelect.append(newOption);
   }
+  renderBookmarksUser();
+
+  userSelect.addEventListener("change", () => {
+    renderBookmarksUser();
+  });
+
+  function getUserId() {
+    return Number(userSelect.value.split(" ").slice(-1)); // get user id
+  }
+
+  const userId = getUserId();
+
+  const addNewBookmarkForm = document.querySelector("#add-new-bookmark");
+  addNewBookmarkForm.addEventListener("submit", (event) => {
+    event.preventDefault();
+
+    const title = document.querySelector("#title").value.trim();
+    const url = document.querySelector("#url").value.trim();
+    const description = document.querySelector("#description").value.trim();
+
+    if (!title || !url || !description) {
+      alert("Please fill in all fields.");
+      return;
+    }
+    let data = getData(userId);
+    console.log(data);
+    if (!data) {
+      data = [{ title: title, url: url, description: description }];
+    } else {
+      data.push({ title: title, url: url, description: description });
+    }
+    setData(userId, data);
+    renderBookmarksUser();
+  });
+
+  const deleteAllButton = document.querySelector("#delete-all-bookmarks");
+  deleteAllButton.addEventListener("click", (event) => {
+      event.preventDefault();
+      clearData(userId);
+      renderBookmarksUser();
+  });
 };
