36131ab323f995a57dd038d0473d0886e953469f
Anna Fedyna <annafedyna03@gmail.com>
2025-02-17 23:18:04 +0000

    refac: moved renderOptions for users

---
+++ renderOptions.js
@@ -0,0 +1,13 @@
+import { getUserIds } from "./storage.js";
+
+export function renderOptions() {
+    const userSelect = document.querySelector("#user-select");
+    const users = getUserIds();
+
+    for (let i = 0; i < users.length; i++) {
+      const newOption = document.createElement("option");
+      newOption.value = i+1;
+      newOption.innerHTML = `User ${users[i]}`;
+      userSelect.append(newOption);
+    }   
+}
