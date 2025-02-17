0c3afa86309438e0f7951ff5ffefe913fb9c7901
Anna Fedyna <annafedyna03@gmail.com>
2025-02-17 23:16:20 +0000

    refac: separated functionality of callbacks

--- script.js
+++ script.js
@@ -1,60 +1,36 @@
-// This is a placeholder file which shows how you can access functions defined in other files.
-// It can be loaded into index.html.
-// You can delete the contents of the file once you have understood how it works.
-// Note that when running locally, in order to open a web page which uses modules, you must serve the directory over HTTP e.g. with https://www.npmjs.com/package/http-server
-// You can't open the index.html file using a file:// URL.
-
-import { getUserIds, getData, setData, clearData } from "./storage.js";
+import { clearData } from "./storage.js";
 import { renderBookmarksUser } from "./renderBookmarks.js";
+import { renderOptions } from "./renderOptions.js";
+import { getUserId } from "./getUserId.js";
+import { addNewBookmark } from "./addNewBookmark.js";
 
 window.onload = function () {
-  const users = getUserIds();
   const userSelect = document.querySelector("#user-select");
-  for (let i = 0; i < users.length; i++) {
-    const newOption = document.createElement("option");
-    newOption.value = i;
-    newOption.innerHTML = `User ${users[i]}`;
-    userSelect.append(newOption);
-  }
-  renderBookmarksUser();
+  let userId;
+  renderOptions();
 
-  userSelect.addEventListener("change", () => {
-    renderBookmarksUser();
+  userSelect.addEventListener("change", (event) => {
+    event.preventDefault();
+    const selectUserMessage = document.querySelector("#select-user-message");
+    selectUserMessage.style.display = "none";
+    userId = getUserId();
+    renderBookmarksUser(userId);
   });
-
-  function getUserId() {
-    return Number(userSelect.value.split(" ").slice(-1)); // get user id
-  }
-
-  const userId = getUserId();
 
   const addNewBookmarkForm = document.querySelector("#add-new-bookmark");
   addNewBookmarkForm.addEventListener("submit", (event) => {
     event.preventDefault();
-
-    const title = document.querySelector("#title").value.trim();
-    const url = document.querySelector("#url").value.trim();
-    const description = document.querySelector("#description").value.trim();
-
-    if (!title || !url || !description) {
-      alert("Please fill in all fields.");
-      return;
-    }
-    let data = getData(userId);
-    console.log(data);
-    if (!data) {
-      data = [{ title: title, url: url, description: description }];
+    if (userId) {
+      addNewBookmark(userId);
     } else {
-      data.push({ title: title, url: url, description: description });
+      alert('Choose an user to add new bookmark!')
     }
-    setData(userId, data);
-    renderBookmarksUser();
   });
 
   const deleteAllButton = document.querySelector("#delete-all-bookmarks");
   deleteAllButton.addEventListener("click", (event) => {
-      event.preventDefault();
-      clearData(userId);
-      renderBookmarksUser();
+    event.preventDefault();
+    clearData(userId);
+    renderBookmarksUser(userId);
   });
 };
