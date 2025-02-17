62ff81fe4d072c861ba5b9ee6ad050bdfadbac3c
Anna Fedyna <annafedyna03@gmail.com>
2025-02-17 23:20:51 +0000

    chor: deleted unused code plus no userdata to render func

--- renderBookmarks.js
+++ renderBookmarks.js
@@ -1,38 +1,36 @@
 import { getData } from "./storage.js";
+import { calculatedaysAgo } from "./calculateDaysAgo.js";
 
-export function renderBookmarksUser() {
-  const userSelect = document.querySelector("#user-select");
-  function getUserId() {
-    return Number(userSelect.value.split(" ").slice(-1)); // get user id
+export function renderBookmarksUser(userId) {
+  const userData = getData(userId);
+  const addNewBookmarkForm = document.querySelector("#add-new-bookmark");
+  const bookmarksContainer = document.querySelector("#bookmarks-container");
+
+  if (userData) {
+    for (const bookmark of userData) {
+      const newBookmark = document.createElement("div");
+
+      const newBookmarkTitle = document.createElement("a");
+      newBookmarkTitle.href = bookmark.url;
+      newBookmarkTitle.textContent = bookmark.title;
+      newBookmarkTitle.target = "_blank";
+
+      const description = document.createElement("p");
+      description.textContent = bookmark.description;
+
+
+      const daysAgo = document.createElement("p");
+      daysAgo.textContent = calculatedaysAgo(bookmark.date);
+
+      newBookmark.appendChild(newBookmarkTitle);
+      newBookmark.appendChild(description);
+      newBookmark.appendChild(daysAgo);
+
+      bookmarksContainer.appendChild(newBookmark);
+    }
+  } else {
+    bookmarksContainer.innerHTML =
+      "It looks like this user hasn't added any bookmarks yet. Start by adding one to keep track of your favorite books!";
+    addNewBookmarkForm.reset();
   }
-
-  const userId = getUserId();
-  const userData = getData(userId); // get user data
-
-    // render
-    if (userData) {
-        const bookmarksContainer = document.querySelector(
-          "#bookmarks-container"
-        );
-        const selectUserMessage = document.querySelector(
-          "#select-user-message"
-        );
-        selectUserMessage.style.display = "none";
-        for (const bookmark of userData) {
-          const newBookmark = document.createElement("div");
-
-          const newBookmarkTitle = document.createElement("a");
-          newBookmarkTitle.href = bookmark.url;
-          newBookmarkTitle.textContent = bookmark.title;
-          newBookmarkTitle.target = "_blank";
-
-          const descriptionPara = document.createElement("p");
-          descriptionPara.textContent = bookmark.description;
-
-          newBookmark.appendChild(newBookmarkTitle);
-          newBookmark.appendChild(descriptionPara);
-
-          bookmarksContainer.appendChild(newBookmark);
-        }
-    }else {}
 }
